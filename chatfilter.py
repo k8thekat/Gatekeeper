9ba9f3ba12f9435f91f74c8bb8f446bb00fd951c
k8thekat <cadwalladerkatelynn@gmail.com>
2022-02-13 14:39:45 -0800

    Adding discord support

--- chatfilter.py
+++ chatfilter.py
@@ -1,11 +1,11 @@
 #Sentinel Bot - Chat filter
 import discord
 
-def scan(content):
+def scan(content,client):
     print(content)
     #Strips Unicode out of the message
     content = unicode(content)
-    content = user(content)
+    content = user(content,client)
     #Attempts to find discord emoji's and remove them
     while(1):
         start_bracket = content.find('<:')
@@ -22,13 +22,13 @@ def unicode(content):
     content = content.decode()
     return content
 
-def user(content):
+def user(content,client):
     user_find_start = content.find('<@!')
     user_find_end = content.find('>',user_find_start)
     if user_find_start != -1 and user_find_end != -1:
         userid = content[user_find_start+3:user_find_end]
         print(userid)
-        username = discord.Client.get_user(id = int(userid))
+        username = client.get_user(id = int(userid))
         #username = 'k8_thekat'
         content = content[0:user_find_start]+ username.name + content[user_find_end+1:]
         return content

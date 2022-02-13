1607f4b9662f806fe0007f1e4948a6f5f3e1f9ad
k8thekat <cadwalladerkatelynn@gmail.com>
2022-02-12 16:36:41 -0800

    Fixed typos

--- SentinelBot.py
+++ SentinelBot.py
@@ -438,14 +438,12 @@ def serverconsole(curdb):
     for server in AMPserverConsoles:
         curserver = curdb.GetServer(server)
         console = AMPserverConsoles[server].ConsoleUpdate()
-        print(console)
         #All AMP functions return False if they are offline...
         if console == False:
             continue
         consolemsg = []
         #Walks through every entry of a Console Update
         for entry in console['ConsoleEntries']:
-            print(curserver.FriendlyName,entry)
             #send off the server chat messages to a discord channel if enabled
             serverchat(curserver,entry)
             #Checks for User last login and updates the database.
@@ -499,6 +497,7 @@ def colorstrip(entry):
             newchar = entry['Contents'][index:index+2]
             entry['Contents'] = entry['Contents'].replace(newchar,'')
         return entry
+    return entry
 
 #Console messages are checked by 'Source' and by 'Type' to be sent to a designated discord channel.
 def serverchat(curserver,entry):

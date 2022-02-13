22d8a08a9b8c19c683c5200ddd89a806b99ef973
k8thekat <cadwalladerkatelynn@gmail.com>
2022-02-13 15:44:13 -0800

    formatting change

--- consolefilters.py
+++ consolefilters.py
@@ -3,14 +3,16 @@ import config
 
 
 def filters(entry):
-    print('Console Filters',entry)
+    #print('Console Filters',entry)
     if config.CommandBlocks:
         if entry['Contents'].startswith('CommandBlock at'):
             #print(f'Console Filter triggered... Removed: {entry["Contents"]}')
             return True
+
     if config.WorldEdit:
         if entry['Contents'].find('issued server command: //') != -1:
             return True
+
     if config.Default:
         if entry['Contents'].startswith('Current Memory Usage:') and entry['Contents'].endswith('mb)'):
             #print(f'Console Filter triggered... Removed: {entry["Contents"]}')
@@ -19,6 +21,7 @@ def filters(entry):
             return True
         if entry['Source'] == 'Server thread/Warn':
             return True
+
     if config.Debugging:
         if entry['Source'] == 'Server thread/ERROR':
             return True

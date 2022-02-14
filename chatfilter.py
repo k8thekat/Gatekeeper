4fb536ad9e6b955ef3da36385dea077d16b25a3d
k8thekat <cadwalladerkatelynn@gmail.com>
2022-02-13 16:08:52 -0800

    typo @ 46

--- chatfilter.py
+++ chatfilter.py
@@ -15,6 +15,7 @@ def scan(content,client):
         else:
             if userstatus == False and emojistatus == False and channelstatus == False:
                 content = content.strip()
+                print(len(content))
                 if len(content) == 0:
                     return True
                 else:
@@ -32,8 +33,8 @@ def user(content,client):
     user_find_end = content.find('>',user_find_start)
     if user_find_start != -1 and user_find_end != -1:
         userid = content[user_find_start+3:user_find_end]
-        username = client.get_user(id = int(userid))
-        content = content[0:user_find_start] + username.name + content[user_find_end+1:]
+        user = client.get_user(id = int(userid))
+        content = content[0:user_find_start] + user.name + content[user_find_end+1:]
         return content
     else:
         return False
@@ -42,9 +43,9 @@ def channel(content,client):
     channel_find_start = content.find('<#')
     channel_find_end = content.find('>',channel_find_start)
     if channel_find_start != -1 and channel_find_end != -1:
-        channelid = content[channel_find_start+3:channel_find_end]
-        channelname = client.get_channel(id = int(channelid))
-        content = content[0:channel_find_start] + channelname.name + content[channel_find_end+1:]
+        channelid = content[channel_find_start+2:channel_find_end]
+        channel = client.get_channel(id = int(channelid))
+        content = content[0:channel_find_start] + channel.name + content[channel_find_end+1:]
         return content
     else:
         return False

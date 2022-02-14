7f1d0d5687b3b8d92b7ee2305b02fb806cde0a96
k8thekat <cadwalladerkatelynn@gmail.com>
2022-02-13 16:06:59 -0800

    added empty message filtering

--- chatfilter.py
+++ chatfilter.py
@@ -5,14 +5,21 @@ def scan(content,client):
     while(1):
         userstatus = user(content,client)
         emojistatus = emoji(content)
+        channelstatus = channel(content,client)
         if userstatus != False:
             content = userstatus  
         elif emojistatus != False:
             content = emojistatus 
+        elif channelstatus != False:
+            content = channelstatus
         else:
-            if userstatus == False and emojistatus == False:
-                return content
-    
+            if userstatus == False and emojistatus == False and channelstatus == False:
+                content = content.strip()
+                if len(content) == 0:
+                    return True
+                else:
+                    return content
+
 #Strips Unicode out of the message
 def unicode(content):
     content = content.encode("ascii","ignore")
@@ -27,6 +34,17 @@ def user(content,client):
         userid = content[user_find_start+3:user_find_end]
         username = client.get_user(id = int(userid))
         content = content[0:user_find_start] + username.name + content[user_find_end+1:]
+        return content
+    else:
+        return False
+
+def channel(content,client):
+    channel_find_start = content.find('<#')
+    channel_find_end = content.find('>',channel_find_start)
+    if channel_find_start != -1 and channel_find_end != -1:
+        channelid = content[channel_find_start+3:channel_find_end]
+        channelname = client.get_channel(id = int(channelid))
+        content = content[0:channel_find_start] + channelname.name + content[channel_find_end+1:]
         return content
     else:
         return False

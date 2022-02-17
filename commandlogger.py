9817763eff1bb4af4277bff2510c6476fb9fa4a1
k8thekat <cadwalladerkatelynn@gmail.com>
2022-02-16 20:21:29 -0800

    Copyright added

--- commandlogger.py
+++ commandlogger.py
@@ -1,4 +1,24 @@
-#command logger
+'''
+   Copyright (C) 2021-2022 Katelynn Cadwallader.
+
+   This file is part of Sentinel, the AMP Minecraft Discord Bot.
+
+   Sentinel is free software; you can redistribute it and/or modify
+   it under the terms of the GNU General Public License as published by
+   the Free Software Foundation; either version 3, or (at your option)
+   any later version.
+
+   Sentinel is distributed in the hope that it will be useful, but WITHOUT
+   ANY WARRANTY; without even the implied warranty of MERCHANTABILITY
+   or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public
+   License for more details.
+
+   You should have received a copy of the GNU General Public License
+   along with Sentinel; see the file COPYING.  If not, write to the Free
+   Software Foundation, 51 Franklin Street - Fifth Floor, Boston, MA
+   02110-1301, USA. 
+'''
+#Sentinel Bot - command logger
 import config
 import os
 import json
